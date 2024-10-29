509c400577bc272c7e153fe33f334414a4fe82d2
RoyMurphy99 <roymurphy99@hotmail.co.uk>
2024-10-29 20:38:05 +0000

    Create self_taught_20.py

---
+++ self_taught_20.py
@@ -0,0 +1,27 @@
+"""
+Python web scraper.
+Chapter 20 of the self taught programmer.
+Roy Murphy 29/10/2024.
+"""
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper():
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser)
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            if url is None:
+                continue
+            if "html" in url:
+                print("\n" + url)
+
+news = "https://news.google.com/"
+Scraper(news).scrape()
